61a7908123482f7668eb75a865c4e63fe773c37f
samangh379 <samangh379@gmail.com>
2022-08-20 15:57:16 +0430

    complete intro part of web

--- src/Components/intro/Intro.js
+++ src/Components/intro/Intro.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./intro.scss";
 
 const Intro = () => {
-    return <div className="intro" id="intro"></div>;
+    return (
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Saman Gholami</h1>
+                    <h3>
+                        Front-End <span>Developer(React)</span>
+                    </h3>
+                    <a href="#portfolio">
+                        <img src="assets/down.png" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Intro;
